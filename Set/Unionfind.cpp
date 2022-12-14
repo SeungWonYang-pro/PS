2bd9d7ddc8ea7e13522a7aa3fba2a8fc175d8087
SeungwonYang <65176721+SeungWonYang-pro@users.noreply.github.com>
2022-12-14 21:49:08 +0900

    Create Unionfind.cpp

---
+++ Set/Unionfind.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int findSet(int a, int *set)
+{
+    while (set[a] != a)
+    {
+        a = set[a];
+    }
+    return a;
+}
+void unionSet(int a, int b, int *set)
+{
+    int setA = findSet(a, set);
+    int setB = findSet(b, set);
+    if (setA != setB)
+    {
+        if (setA < setB)
+        {
+            set[setB] = setA;
+        }
+        else
+        {
+            set[setA] = setB;
+        }
+    }
+}
+
+int main(void)
+{
+    int n, m;
+    scanf("%d %d", &n, &m);
+    int *set = new int[n + 1];
+    for (int i = 0; i <= n; i++)
+    {
+        set[i] = i;
+    }
+
+    for (int i = 0; i < m; i++)
+    {
+        int cmd, a, b;
+        scanf("%d %d %d", &cmd, &a, &b);
+        if (cmd == 0)
+        {
+            if (a != b)
+            {
+                unionSet(a, b, set);
+            }
+        }
+        else if (cmd == 1)
+        {
+            if (a != b)
+            {
+                int setA = findSet(a, set);
+                int setB = findSet(b, set);
+                if (setA == setB)
+                {
+                    printf("YES\n");
+                }
+                else
+                {
+                    printf("NO\n");
+                }
+            }
+            else
+            {
+                printf("YES\n");
+            }
+        }
+    }
+
+    delete[] set;
+
+    return 0;
+}
